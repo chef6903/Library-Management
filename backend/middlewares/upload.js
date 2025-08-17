e3a2792741deb811824d07a77b986e052a0e0846
hieu6903 <hieu6903az@gmail.com>
2025-08-17 17:01:41 +0700

    fix route

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -1,24 +1,24 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = [
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        'application/vnd.ms-excel'
-    ];
+  const allowedTypes = [
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.ms-excel",
+  ];
 
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error('Only Excel files are allowed (.xlsx, .xls)'), false);
-    }
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only Excel files are allowed (.xlsx, .xls)"), false);
+  }
 };
 
 const upload = multer({
-    storage,
-    fileFilter,
-    limits: { fileSize: 2 * 1024 * 1024 }
+  storage,
+  fileFilter,
+  limits: { fileSize: 2 * 1024 * 1024 },
 });
 
 module.exports = upload;
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middlewares/Upload");
+const upload = require("../middlewares/upload");
 const jwtConfig = require("../config/jwtconfig");
 const authController = require("../controller/authController");
 
